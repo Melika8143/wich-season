c0a2c087b04c4bab43341b4ba20e8df9ceae7a44
Melika8143 <ranjbaranmelika@gmail.com>
2026-02-22 12:07:55 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    Console.Write("Shomare Fasl: ");
+    var fasl=int.Parse(Console.ReadLine()??"0");
+    if (fasl==1)
+    {
+        Console.WriteLine("Bahar");
+    }
+    if (fasl==2)
+    {
+        Console.WriteLine("Tabestan");
+    }
+    if (fasl==3)
+    {
+        Console.WriteLine("Paeez");
+    }
+    if (fasl==4)
+    {
+        Console.WriteLine("Zemestan");
+
+    }
+    if (fasl<1 || fasl>4)
+    {
+        Console.WriteLine("Nadarim");
+    }
+}
+catch 
+{
+    
+    Console.WriteLine("Error");
+}
